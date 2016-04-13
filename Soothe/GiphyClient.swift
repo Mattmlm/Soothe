b462d35bd70c9381d88e0e9ce83a979b0788b69e
venegu <venegu@gmail.com>
2016-04-13 17:23:00 -0400

    Adding temporary giphy

--- Soothe/GiphyClient.swift
+++ Soothe/GiphyClient.swift
@@ -6,4 +6,83 @@
 //  Copyright © 2016 Gale. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+let delegate = UIApplication.sharedApplication().delegate as! AppDelegate
+
+class GiphyClient {
+    
+    let publicKey = delegate.retrieveKeys("giphyKey")
+    var searchTerm = " "
+    
+    var loginCompletion: ((gif: Gif?, error: NSError?) -> ())?
+    var gifs: [Gif]?
+    
+    func getSearchWithCompletion(completion: (gifs: [Gif]?, error: NSError?) -> ()) {
+        let url = NSURL(string:"http://api.giphy.com/v1/gifs/search?q=\(searchTerm)&api_key=\(publicKey)")
+        
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                        
+                            if (responseDictionary["data"] != nil ) {
+                                
+                                self.gifs = Gif.gifsWithArray(responseDictionary["data"] as! [NSDictionary])
+                                completion(gifs: self.gifs, error: nil)
+                            } else {
+                                print(error)
+                                completion(gifs: nil, error: error)
+                            }
+                    } else {
+                        completion(gifs: nil, error: error)
+                    }
+                } else {
+                    completion(gifs: nil, error: error)
+                }
+        });
+        task.resume()
+    }
+    
+    func getTrendingWithCompletion(completion: (gifs: [Gif]?, error: NSError?) -> ()) {
+        let url = NSURL(string:"http://api.giphy.com/v1/gifs/trending?api_key=\(publicKey)")
+        
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            //print(responseDictionary)
+                            if (responseDictionary["data"] != nil ) {
+                                print(responseDictionary["data"])
+                                self.gifs = Gif.gifsWithArray(responseDictionary["data"] as! [NSDictionary])
+                                
+                                print("Connection to API successful!")
+                                completion(gifs: self.gifs, error: nil)
+                            }
+                            else {
+                                print("error")
+                                completion(gifs: nil, error: error)
+                            }
+                    } else { completion(gifs: nil, error: error) }
+                } else { completion(gifs: nil, error: error) }
+        });
+        task.resume()
+    }
+
+}
